c3299a97e416806c547991b492b035e197da5c3d
José Luis de la Hoz <joseluisdelahozgarcia@gmail.com>
2018-10-22 19:43:24 +0200

    buy stock 1 error solved

--- src/controller/BuyController.java
+++ src/controller/BuyController.java
@@ -23,7 +23,7 @@ public class BuyController extends Controller{
 		if(view.getId() != 0) {
 			for(int i = 0; i < search.size(); i++) {
 				if(view.getId() == search.get(i).getProductId() && search.get(i).availableStock()) {
-					if(search.get(i).getProductStock() - view.getAmount() > 0) {
+					if(search.get(i).getProductStock() - view.getAmount() >= 0) {
 						flag = true;
 						model.buyProduct(search.get(i), view.getAmount());
 						break;
